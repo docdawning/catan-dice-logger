4c678710c28a93bdc8b84035d990e3a5b09269b9
j <j@10.197.253.164>
2017-07-30 17:30:11 -0600

    Various changes. Nothing too special.

--- CatanDiceLogger/src/CatanDiceLogger.java
+++ CatanDiceLogger/src/CatanDiceLogger.java
@@ -1,23 +1,24 @@
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.util.Scanner;
 import java.util.HashMap;
 
-class DiceKajigger {
+class CatanDiceLogger {
 	private static final String EPISODE_PREFIX="episode";
 	private static final String EPISODE_POSTFIX=".txt";
 	
 	public static void main(String[] args) {
-		DiceKajigger dk = new DiceKajigger();
-		dk.go(args);
+		CatanDiceLogger app = new CatanDiceLogger();
+		app.go(args);
 	}
 
 	private void go(String[] args) {
 		if (args.length < 1) {
-			performDataCollection(getNextEpisodeFile(), true, true);
+			performDataCollection(getNextEpisodeFile(), true);
 		} else {
-			File possibleExistingEpisodeFile = new File(args[1]);
-			if (possibleExistingEpisodeFile.exists()) performDataCollection(possibleExistingEpisodeFile, false, false);
+			File possibleExistingEpisodeFile = new File(args[0]);
+			if (possibleExistingEpisodeFile.exists()) performDataCollection(possibleExistingEpisodeFile, false);
 			else {
 				System.out.println("Gave parameters, but like.. The first one isn't a file and that's the only thing I know how to act upon.");
 			}
@@ -25,7 +26,7 @@ class DiceKajigger {
 		}
 	}
 
-	private void performDataCollection(File episodeFile, boolean writeFile, boolean promptForInput) {
+	private void performDataCollection(File episodeFile, boolean writeFile) {
 		HashMap<Integer,Integer> statMap = new HashMap<Integer,Integer>();
 		System.out.println("Recording to episode file: "+episodeFile.getName());
 		boolean running = true;
@@ -33,34 +34,18 @@ class DiceKajigger {
 		System.out.println("Entering an empty line will end this episode, otherwise input samples one per line");
 		int N = 0;
 		float avg = 0;
-		int lastSample = 0;
 		while (running) {
 			String input = in.nextLine();
 			if (input.equals("")) running = false;
 			else {
 				int sample = Integer.parseInt(input.replaceAll("[\\D]", ""));
 				N++;
-				//update rolling average
-				if (avg == 0) {
-					avg = sample;
-				}
-				else {
-					avg -= (lastSample / N);
-					avg += (sample / N);
-				} 
-				lastSample = sample;
-				//System.out.println("Avg: "+avg);
-				writeSampleToFile(sample, episodeFile);
-		
-				Integer key = new Integer(sample);
-				Integer keyValue = statMap.get(key);
-				if (keyValue == null) statMap.put(key, new Integer(1));
-				else {
-					statMap.put(key, new Integer(keyValue.intValue() + 1));
-				}
-				
+				avg = updateRunningAverage(N, avg, sample); 
+				if (writeFile) writeSampleToFile(sample, episodeFile);
+				recordStats(statMap, sample);
 			}
 		}
+		in.close();
 		System.out.println("\nThere were "+N+" samples, with an average of: "+avg);
 		for (Integer key : statMap.keySet()) {
 			System.out.println(key+": "+statMap.get(key)+" occurences");
@@ -68,12 +53,38 @@ class DiceKajigger {
 		System.out.println("The end");
 	}
 
+	private void recordStats(HashMap<Integer, Integer> statMap, int sample) {
+		Integer key = new Integer(sample);
+		Integer keyValue = statMap.get(key);
+		if (keyValue == null) statMap.put(key, new Integer(1));
+		else {
+			statMap.put(key, new Integer(keyValue.intValue() + 1));
+		}
+	}
+
+	//Using Wellford's Running Average Method: 
+	//https://stackoverflow.com/questions/12636613/how-to-calculate-moving-average-without-keeping-the-count-and-data-total
+	private float updateRunningAverage(int N, float avg, int sample) {
+		if (N == 1) avg = sample;
+		else avg = avg + ((sample-avg)/N);
+		
+		//System.out.println("Running average: "+avg+": N="+N+", sample="+sample);
+		return avg;
+	}
+
 	private void writeSampleToFile(int sample, File episodeFile) {
+		Integer integer = new Integer(sample);
 		try {
+			/*
 			FileWriter fw = new FileWriter(episodeFile);
-			Integer integer = new Integer(sample);
 			fw.write(integer.toString());
 			fw.close();
+			*/
+			
+			BufferedWriter writer = new BufferedWriter(new FileWriter(episodeFile, true));
+			writer.write(integer.toString()+"\n");
+			writer.flush();
+			writer.close();
 		} catch (Exception e) {
 			System.out.println("Couldn't write \""+sample+"\" to file "+episodeFile.getName());
 		}
--- run.sh
+++ run.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
-MAIN_NAME=DiceKajigger
-javac $MAIN_NAME.java
-java $MAIN_NAME $1 $2 $3 $4 $5 $6
+SRC_DIR=CatanDiceLogger/src
+MAIN_CLASS=CatanDiceLogger
+javac -d build/ $SRC_DIR/$MAIN_CLASS.java
+if [ "$?" -ne 0 ] ; then
+	echo -e "\nBuild failed. Halting"
+	exit -1
+fi
+
+echo -e "\nBuild succeeded. Running\n\n"
+java -cp build/ $MAIN_CLASS $1 $2 $3 $4 $5 $6
+
